126e1bab7d61c0c55ee9f222724523abdba6657b
Hariharanrs_0828 <57134303+hariTH-0828@users.noreply.github.com>
2023-02-04 23:07:32 +0530

    Local changes

--- app/src/main/java/edu/mobile/voting/MainActivity.java
+++ app/src/main/java/edu/mobile/voting/MainActivity.java
@@ -3,22 +3,34 @@ package edu.mobile.voting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button validateId;
+    private TextView validateId;
+    private TextInputEditText getVoterid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        validateId = findViewById(R.id.idValidate);
+        validateId = findViewById(R.id.idVerify);
+        getVoterid = findViewById(R.id.editVoterid);
 
-        validateId.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Button clicked", Toast.LENGTH_SHORT).show());
+        getVoterid.setOnFocusChangeListener((view, b) -> {
+            if(!b){
+                Log.i("getVoter", "Focus out");
+            }
+        });
+
+        validateId.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "Button clicked", Toast.LENGTH_SHORT).show());
     }
 
 
---
+++ app/src/main/java/edu/mobile/voting/ValidateOTP.java
@@ -0,0 +1,14 @@
+package edu.mobile.voting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ValidateOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validate_otp);
+    }
+}
